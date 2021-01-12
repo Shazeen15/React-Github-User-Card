ffc760d733d2e59bdbad6c9a09aecfe7e742fce6
Shazeen Fabius <shazeenfabius@Shazeens-Mini.lan>
2021-01-12 13:52:55 -0800

    displaying github profile information

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import React from "react";
+import axios from "axios";
 import "./App.css";
+import Github from "./components/Github";
 
-function App() {
-  return (
-    <div>
-      <h1>github</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    gitHub: {},
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/shazeen15")
+      .then((res) => {
+        this.setState({
+          gitHub: res.data,
+        });
+        console.log(this.state.gitHub);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <Github gitHub={this.state.gitHub} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Github.js
+++ src/components/Github.js
@@ -4,8 +4,21 @@ class Github extends React.Component {
   render() {
     return (
       <div>
-        <h1>card</h1>
+        <img src={this.props.gitHub.avatar_url} alt="shazeen profile img" />
+        <h1>{this.props.gitHub.name}</h1>
+        <h2>{this.props.gitHub.bio}</h2>
+        <p>{this.props.gitHub.location}</p>
+        <p>{this.props.gitHub.login}</p>
+        <a href={this.props.gitHub.blog} target="_blank" rel="noreferrer">
+          Blog
+        </a>
+        <p>Followers: {this.props.gitHub.followers}</p>
+        <p>Following: {this.props.gitHub.following}</p>
+        <p>Repos: {this.props.gitHub.public_repos}</p>
+        <p>Twitter: {this.props.gitHub.twitter_username}</p>
       </div>
     );
   }
 }
+
+export default Github;
